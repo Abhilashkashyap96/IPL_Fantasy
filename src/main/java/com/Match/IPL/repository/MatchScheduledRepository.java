eaccc7d14086f0c591fb71fd377a5084517a5c6f
Abhilashkashyap96 <abhilashkashyap.123456@gmail.com>
2022-03-27 15:03:32 +0530

    IPL Fantasy project | Refactored

---
+++ src/main/java/com/Match/IPL/repository/MatchScheduledRepository.java
@@ -0,0 +1,16 @@
+package com.Match.IPL.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Match.IPL.entity.MatchScheduleEntity;
+
+
+@Repository
+public interface MatchScheduledRepository extends JpaRepository<MatchScheduleEntity,Integer> {
+
+	void save(Optional<MatchScheduleEntity> findById);
+
+}
